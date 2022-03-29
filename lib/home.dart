d2fe6c3aae3a46d22ff3ae0f44313654ad91ae5e
Rizky Agung Ramadhan <rizkyagungramadhan@gmail.com>
2022-03-29 22:42:19 +0700

    add fade switch transition on compose & reply fab icon

--- lib/home.dart
+++ lib/home.dart
@@ -714,6 +714,7 @@ class _ReplyFabState extends State<_ReplyFab>
     with SingleTickerProviderStateMixin {
   // TODO: Add Fade through transition between compose and reply FAB (Motion)
   static const double _mobileFabDimension = 56;
+  static final fabKey = UniqueKey();
 
   @override
   Widget build(BuildContext context) {
@@ -724,20 +725,23 @@ class _ReplyFabState extends State<_ReplyFab>
       selector: (context, emailStore) => emailStore.onMailView,
       builder: (context, onMailView, child) {
         // TODO: Add Fade through transition between compose and reply FAB (Motion)
-        final fabSwitcher = onMailView
-            ? const Icon(
-                Icons.reply_all,
-                color: Colors.black,
-              )
-            : const Icon(
-                Icons.create,
-                color: Colors.black,
-              );
+        final fabSwitcher = _FadeThroughTransitionSwitcher(
+            child: onMailView
+                ? Icon(
+                    Icons.reply_all,
+                    key: fabKey,
+                    color: Colors.black,
+                  )
+                : const Icon(
+                    Icons.create,
+                    color: Colors.black,
+                  ),
+            fillColor: Colors.transparent);
         final tooltip = onMailView ? 'Reply' : 'Compose';
 
         // TODO: Add Container Transform from FAB to compose email page (Motion)
         return OpenContainer(
-          openBuilder : (context, closedContainer) {
+          openBuilder: (context, closedContainer) {
             return const ComposePage();
           },
           openColor: theme.cardColor,
@@ -776,3 +780,25 @@ class _ReplyFabState extends State<_ReplyFab>
 }
 
 // TODO: Add Fade through transition between compose and reply FAB (Motion)
+class _FadeThroughTransitionSwitcher extends StatelessWidget {
+  const _FadeThroughTransitionSwitcher(
+      {required this.child, required this.fillColor});
+
+  final Widget child;
+  final Color fillColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageTransitionSwitcher(
+      transitionBuilder: (child, animation, secondaryAnimation) {
+        return FadeThroughTransition(
+          animation: animation,
+          child: child,
+          secondaryAnimation: secondaryAnimation,
+          fillColor: fillColor,
+        );
+      },
+      child: child,
+    );
+  }
+}
